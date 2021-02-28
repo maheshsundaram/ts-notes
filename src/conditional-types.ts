9e752a31ec464185212f4f59086583adb4ec0422
Mahesh Sundaram <mahesh@omkara.dev>
2021-02-27 21:40:25 -1000

    wip

---
+++ src/conditional-types.ts
@@ -0,0 +1,10 @@
+// conditional types
+// the inputs of a conditional type are types, and the outputs are also a type
+//
+// are they then pure functions?
+//
+// infer creates a type variable
+//
+// string extends T
+//
+// what does that do?
