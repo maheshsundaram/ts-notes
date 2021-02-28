adc4f24cb8b9bc40271e8fd38c9a820775507713
Mahesh Sundaram <mahesh@omkara.dev>
2021-02-28 00:39:48 -1000

    begin tokenize

--- src/white-hibiscus.ts
+++ src/white-hibiscus.ts
@@ -1,6 +1,31 @@
 import { car, cdr, cons } from "./red-hibiscus"
 import { split } from "./utils"
 
+// balance parens
+// handle spaces
+// use recursive types
+
+export type trim<t> = t extends ` ${infer rest}` ? trim<rest> : t;
+
+type tokenize<t> =
+  trim<t> extends `${infer head} ${infer tail}` ? [head, tail] :
+  // trim<t> extends `${infer head},${infer tail}` ? [head, tail] :
+  trim<t> extends `(${infer head})` ? [head] :
+  trim<t> extends `(${infer head}${infer tail})` ? [head, tail] :
+  trim<t> extends `${infer head})${infer tail}` ? [head, tail] :
+  trim<t> extends `${infer head};${infer tail}` ? [head, tail] :
+  trim<t> extends `${infer head})` ? [head, ')'] :
+  trim<t> extends `${infer head};` ? [head, ';'] :
+  [trim<t>, '']
+
+type t1 = tokenize<"(car)">
+type t2 = tokenize<"(car '())">
+type t3 = tokenize<"(car '(one))">
+type t4 = tokenize<"(car '(one two))">
+type t5 = tokenize<"(cdr '())">
+type t6 = tokenize<"(cdr '(one))">
+type t7 = tokenize<"(cdr '(one two))">
+
 export type parse<expr extends string> =
   expr extends `()` ? expr :
   expr extends `'()` ? expr :
