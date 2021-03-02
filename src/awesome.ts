f8c4d3dc7c0debc432a4c87f0c015f4633b37fd5
Mahesh Sundaram <mahesh@omkara.dev>
2021-03-01 19:07:52 -1000

    awesome

--- src/awesome.ts
+++ src/awesome.ts
@@ -7,3 +7,6 @@
 // "How to master advanced TypeScript patterns
 //    Learn how to create types for curry and Ramda"
 // https://github.com/millsp/medium/blob/master/types-curry-ramda/src/index.ts
+//
+// "Emulating a 4-Bit Virtual Machine in Typescript"
+// https://gist.github.com/acutmore/9d2ce837f019608f26ff54e0b1c23d6e
