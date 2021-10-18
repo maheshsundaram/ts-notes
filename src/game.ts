0ed92902533b36a5c99d9b02c5d4a564b503287c
Mahesh Sundaram <mahesh@omkara.dev>
2021-10-18 10:11:13 -1000

    Upgrade TS

--- src/game.ts
+++
@@ -1,4 +0,0 @@
-type Int = "o" | "" | "x";
-type Row = [Int, Int, Int];
-type Board = [Row, Row, Row];
-type GameStart = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
--- src/no-infer-type-params.ts
+++ src/no-infer-type-params.ts
@@ -1,2 +1,20 @@
 // https://github.com/Microsoft/TypeScript/issues/14829
 export type NoInfer<T> = T & { [K in keyof T]: T[K] };
+
+class Animal {
+  move: unknown;
+}
+class Dog extends Animal {
+  woof: unknown;
+}
+
+declare function doSomething<T>(value: T, getDefault: () => T): void;
+
+// Wanted an error here - getDefault() ought to return same type as 'value'
+doSomething(new Dog(), () => new Animal());
+
+declare function assertEqual<T>(actual: T, expected: T): boolean;
+
+const g = { x: 3, y: 2 };
+
+// fixed: assertEqual(g, { x: 3 }); // Forgot y, wanted error
