12f2a5c514ef4f4636f4abb00ea67e5182e00730
Mahesh Sundaram <mahesh@omkara.dev>
2021-10-19 12:35:50 -1000

    Add memoized fib

---
+++ src/memoized-fib.ts
@@ -0,0 +1,54 @@
+// prettier-ignore
+type MemoizedFib<N extends number, List extends number[][] = [[1], [1], [1, 1]]> =
+  N extends 1
+    ? 1
+    : N extends 2
+      ? 1
+      : N extends 3
+        ? 2
+        : List["length"] extends N
+          ? List extends [...infer _, infer T]
+            ? T extends number[]
+              ? T["length"]
+              : never
+            : never 
+          : List extends [...infer H, infer X, infer Y]
+            ? X extends number[]
+              ? Y extends number[]
+                ? H extends number[][]
+                  ? MemoizedFib<N, [...H, X, Y, [...X, ...Y]]>
+                  : never
+                : never
+              : never
+            : never;
+
+// prettier-ignore
+type Index = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+
+type First20Fibs = {
+  [K in Index]: MemoizedFib<K>;
+};
+// Is equal to:
+//
+// type First20Fibs = {
+//     1: 1;
+//     2: 1;
+//     3: 2;
+//     4: 3;
+//     5: 5;
+//     6: 8;
+//     7: 13;
+//     8: 21;
+//     9: 34;
+//     10: 55;
+//     11: 89;
+//     12: 144;
+//     13: 233;
+//     14: 377;
+//     15: 610;
+//     16: 987;
+//     17: 1597;
+//     18: 2584;
+//     19: 4181;
+//     20: 6765;
+// }
