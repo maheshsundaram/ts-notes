b096c2697a174fde73e5c10b32ff1c2e7656b498
Mahesh Sundaram <mahesh@omkara.dev>
2021-10-19 21:18:57 -1000

    awesome

--- src/awesome.ts
+++ src/awesome.ts
@@ -1,9 +1,13 @@
+// "A menagerie of useful type operators for TypeScript"
 // https://github.com/pelotom/type-zoo
 //
+// "A SQL database implemented purely in TypeScript type annotations."
 // https://github.com/codemix/ts-sql
 //
+// "Functions and algorithms implemented purely with TypeScript's type system"
 // https://github.com/ronami/meta-typing
 //
+// "JSON Parser written entirely in TypeScript's type system"
 // https://github.com/jamiebuilds/json-parser-in-typescript-very-bad-idea-please-dont-use/
 //
 // "How to master advanced TypeScript patterns
@@ -13,16 +17,22 @@
 // "Emulating a 4-Bit Virtual Machine in Typescript"
 // https://gist.github.com/acutmore/9d2ce837f019608f26ff54e0b1c23d6e
 //
+// Play Tic Tac Toe in the type system
 // https://blog.joshuakgoldberg.com/type-system-game-engines/
 //
+// "Collection of TypeScript type challenges"
 // https://github.com/type-challenges/type-challenges
 //
+// "Collection of wonderful TypeScript type gymnastics code snippets."
 // https://github.com/g-plane/type-gymnastics
 //
-// https://itnext.io/the-art-of-type-programming-cfd933bdfff7
-//
+// "TypeType is designed to generate complex typescript type with ease."
 // https://github.com/mistlog/typetype
+// https://itnext.io/the-art-of-type-programming-cfd933bdfff7
 //
 // "Proof that TypeScript's Type System is Turing Complete"
 // https://gist.github.com/hediet/63f4844acf5ac330804801084f87a6d4
 // https://github.com/microsoft/TypeScript/issues/14833
+//
+// "Sokoban Game in Pure TypeScript Type System"
+// https://github.com/fc01/TypeGame
