f0dedec0d2e0619d31063b10e2363c0adcb371e3
Mahesh Sundaram <mahesh@omkara.dev>
2022-11-16 16:41:59 -1000

    Format

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,18 @@
+// prettier-ignore
 export type join<T extends unknown[], D extends string> =
   T extends [] ? '' :
     T extends [string] ? `${T[0]}` :
       T extends [string, ...infer U] ? `${T[0]}${D}${join<U, D>}` :
         string;
 
+// prettier-ignore
 export type split<S extends string, D extends string> =
   string extends S ? string[] :
     S extends '' ? [] :
       S extends `${infer T}${D}${infer U}` ? [T, ...split<U, D>] :
         [S];
 
+// prettier-ignore
 export type filter<items extends unknown[], value> =
   items extends []
     ? []
@@ -17,5 +20,4 @@ export type filter<items extends unknown[], value> =
       ? head extends value
         ? filter<tail, value>
         : [head, ...filter<tail, value>]
-      : items; 
-
+      : items;
