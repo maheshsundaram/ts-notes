1fb9adf48c36017774b33126a8404612de9ed49e
Mahesh Sundaram <mahesh@omkara.dev>
2022-11-17 21:11:44 -1000

    Add car-cdr-cons

---
+++ src/car-cdr-cons.ts
@@ -0,0 +1,20 @@
+import { TestEq } from "./test-utils";
+
+// prettier-ignore
+export type Car<List extends unknown[]> =
+  List extends [infer Head, ...infer _]
+    ? Head
+    : List;
+
+// prettier-ignore
+export type Cdr<List extends unknown[]> =
+  List extends [infer _, ...infer Tail]
+    ? Tail
+    : List;
+
+// prettier-ignore
+export type Cons<Item, List extends unknown[] = []> = [Item, ...List];
+
+type Test1 = TestEq<Car<["a", "b", "c"]>, "a">;
+type Test2 = TestEq<Cdr<["a", "b", "c"]>, ["b", "c"]>;
+type Test3 = TestEq<Cons<"a", ["b", "c"]>, ["a", "b", "c"]>;
